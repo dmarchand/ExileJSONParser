d236f25ce07839a51fce79a01befd4e35d3a1f59
Dan Marchand <dan.marchand@gmail.com>
2014-08-14 20:42:03 -0400

    Build node graph!

--- skilltree/tree.js
+++ skilltree/tree.js
@@ -1,7 +1,43 @@
 
 
+
+
+var me = this;
+
+me.skillTree = new Graph();
+
+// Parse json first
 $.getJSON("https://dl.dropboxusercontent.com/u/52921017/treejson.js",function(result){
     $.each(result.nodes, function(i, field){
-        $("div").append(field.dn + "<br/> ");
+        //$("div").append(field.dn + "<br/> ");
+
+        var node = me.skillTree.addNode(field.id);
+        node.descriptiveName=field.dn;
+
+        // Find all the effects and add to node
+        $.each(field.sd, function(i, sd) {
+            node.effects.push(sd);
+        });
+
+        // Find all the connected nodes so we can connect the graph later
+        $.each(field.out, function(i, adj) {
+            node.adjacencyIds.push(adj);
+        });
+
+        me.connectTree();
     });
 });
+
+
+// Magic graph connector, quite slow because it's dumb
+this.connectTree = function() {
+    var nodes = me.skillTree.getAllNodes();
+    $.each(nodes, function (i, node) {
+        $.each(node.adjacencyIds, function(i, adj) {
+            var index = nodes.indexOf(adj);
+            var neighbor = nodes[index];
+            node.addEdge(neighbor, 0);
+            console.log("Added edge: " + adj);
+        });
+    });
+}
